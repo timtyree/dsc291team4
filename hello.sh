425cba1a83347a0a2e9fe267862dbf81351c173b
Tim Tyree <tjt57@cornell.edu>
2020-04-18 17:30:27 -0700

    copied relevant files for experiment
    from github.com/yoavfreund/Public-DSC291/

--- hello.sh
+++ hello.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/bash
-hello.sh
+# echo "Hello"
+#TODO: git_pull_then_push. (later) support keyword argument for "class" ( -c flag) and ""
+#TODO: make any of the file system listed in .gitignore
+#TODO: automate the display of jupyter notebook and a current task list .md file.  
+# re: ^that, this looks fancy, 
+# 
