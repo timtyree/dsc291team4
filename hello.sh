ece27efea78eca13df4d6a6579585ed8052ce3ab
Tim Tyree <tjt57@cornell.edu>
2020-04-19 18:32:36 -0700

    added files
    added lib in HW2/python
    added added to hello.sh
    added outputs for random_pokes.py --> stats.pkl

--- hello.sh
+++ hello.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/bash
-# echo "Hello"
+echo "Hello"
 #TODO: git_pull_then_push. (later) support keyword argument for "class" ( -c flag) and ""
 #TODO: make any of the file system listed in .gitignore
 #TODO: automate the display of jupyter notebook and a current task list .md file.  
 # re: ^that, this looks fancy, 
 # 
+echo 'connect to s3 channels here:'
+echo 'http://localhost:8888/notebooks/Pubic-DSC291-Team-4/notebooks/Section1-Basics/0.MemoryLatency/test-S3-connection.ipynb'
+echo 'run the random_pokes experiment here:'
+echo 'Pubic-DSC291-Team-4/notebooks/Section1-Basics/0.MemoryLatency/random_poke_results.ipynb'
