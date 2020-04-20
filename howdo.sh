6952f7bc99d496aa2ce141a20817864a5955ecb0
Tim Tyree <tjt57@cornell.edu>
2020-04-20 10:34:57 -0700

    added fig_a_highlighted

---
+++ howdo.sh
@@ -0,0 +1,5 @@
+
+
+echo 'connect to s4 channels here:'
+echo 'http://localhost:8888/notebooks/Pubic-DSC291-Team-4/notebooks/Section1-Basics/0.MemoryLatency/test-S3-connection.ipynb'
+echo 'connect to s4 channels here:'
